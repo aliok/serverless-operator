79bf26dc0ccab2e813e6c49ab6aea0fc189c42c8
Ali Ok <aliok@redhat.com>
2020-10-20 14:45:35 +0300

    Have scopes in image overrides

--- hack/generate/csv.sh
+++ hack/generate/csv.sh
@@ -13,56 +13,74 @@ serving="${registry}/knative-v$(metadata.get dependencies.serving):knative-servi
 eventing="${registry}/knative-v$(metadata.get dependencies.eventing):knative-eventing"
 eventing_contrib="${registry}/knative-v$(metadata.get dependencies.eventing_contrib):knative-eventing-sources"
 
-declare -a images
-declare -A images_addresses
+declare -a serving_images
+declare -a eventing_images
+declare -a kafka_images
+declare -A serving_images_addresses
+declare -A eventing_images_addresses
+declare -A kafka_images_addresses
 
-function image {
+function serving_image {
   local name address
   name="${1:?Pass a image name as arg[1]}"
   address="${2:?Pass a image address as arg[2]}"
-  images+=("${name}")
-  images_addresses["${name}"]="${address}"
+  serving_images+=("${name}")
+  serving_images_addresses["${name}"]="${address}"
 }
 
-image "queue-proxy"    "${serving}-queue"
-image "activator"      "${serving}-activator"
-image "autoscaler"     "${serving}-autoscaler"
-image "autoscaler-hpa" "${serving}-autoscaler-hpa"
-image "controller"     "${serving}-controller"
-image "webhook"        "${serving}-webhook"
-image "storage-version-migration-serving-$(metadata.get dependencies.serving)__migrate" "${serving}-storage-version-migration"
+function eventing_image {
+  local name address
+  name="${1:?Pass a image name as arg[1]}"
+  address="${2:?Pass a image address as arg[2]}"
+  eventing_images+=("${name}")
+  eventing_images_addresses["${name}"]="${address}"
+}
 
-image "3scale-kourier-gateway" "docker.io/maistra/proxyv2-ubi8:$(metadata.get dependencies.maistra)"
-image "3scale-kourier-control" "${registry}/knative-v$(metadata.get dependencies.kourier):kourier"
+function kafka_image {
+  local name address
+  name="${1:?Pass a image name as arg[1]}"
+  address="${2:?Pass a image address as arg[2]}"
+  kafka_images+=("${name}")
+  kafka_images_addresses["${name}"]="${address}"
+}
 
-image "eventing-controller__eventing-controller"    "${eventing}-controller"
-image "sugar-controller__controller"                "${eventing}-sugar-controller"
-image "eventing-webhook__eventing-webhook"          "${eventing}-webhook"
-image "storage-version-migration-eventing__migrate" "${eventing}-storage-version-migration"
-image "mt-broker-controller__mt-broker-controller"  "${eventing}-mtchannel-broker"
-image "mt-broker-filter__filter"                    "${eventing}-mtbroker-filter"
-image "mt-broker-ingress__ingress"                  "${eventing}-mtbroker-ingress"
-image "imc-controller__controller"                  "${eventing}-channel-controller"
-image "imc-dispatcher__dispatcher"                  "${eventing}-channel-dispatcher"
+serving_image "queue-proxy"    "${serving}-queue"
+serving_image "activator"      "${serving}-activator"
+serving_image "autoscaler"     "${serving}-autoscaler"
+serving_image "autoscaler-hpa" "${serving}-autoscaler-hpa"
+serving_image "controller"     "${serving}-controller"
+serving_image "webhook"        "${serving}-webhook"
+serving_image "storage-version-migration-serving-$(metadata.get dependencies.serving)__migrate" "${serving}-storage-version-migration"
 
-image "v0.17.0-pingsource-cleanup__pingsource" "${eventing}-pingsource-cleanup"
-image "PING_IMAGE"           "${eventing}-ping"
-image "MT_PING_IMAGE"        "${eventing}-mtping"
-image "APISERVER_RA_IMAGE"   "${eventing}-apiserver-receive-adapter"
-image "BROKER_INGRESS_IMAGE" "${eventing}-broker-ingress"
-image "BROKER_FILTER_IMAGE"  "${eventing}-broker-filter"
-image "DISPATCHER_IMAGE"     "${eventing}-channel-dispatcher"
-image "KN_CLI_ARTIFACTS"     "${registry}/knative-v$(metadata.get dependencies.cli):kn-cli-artifacts"
+serving_image "3scale-kourier-gateway" "docker.io/maistra/proxyv2-ubi8:$(metadata.get dependencies.maistra)"
+serving_image "3scale-kourier-control" "${registry}/knative-v$(metadata.get dependencies.kourier):kourier"
 
-image "kafka-controller-manager__manager"    "${eventing_contrib}-kafka-source-controller"
-image "KAFKA_RA_IMAGE"                       "${eventing_contrib}-kafka-source-adapter"
-image "kafka-ch-controller__controller"      "${eventing_contrib}-kafka-channel-controller"
-# TODO: clash!
-# TODO: we have a separate Kafka dispatcher deployment for the global dispatcher
-# TODO: following image will only be used in a namespaced dispatcher
-image "DISPATCHER_IMAGE"                     "${eventing_contrib}-kafka-channel-dispatcher"
-image "kafka-ch-dispatcher__dispatcher"      "${eventing_contrib}-kafka-channel-dispatcher"
-image "kafka-webhook__kafka-webhook"         "${eventing_contrib}-kafka-channel-webhook"
+serving_image "KN_CLI_ARTIFACTS"     "${registry}/knative-v$(metadata.get dependencies.cli):kn-cli-artifacts"
+
+eventing_image "eventing-controller__eventing-controller"    "${eventing}-controller"
+eventing_image "sugar-controller__controller"                "${eventing}-sugar-controller"
+eventing_image "eventing-webhook__eventing-webhook"          "${eventing}-webhook"
+eventing_image "storage-version-migration-eventing__migrate" "${eventing}-storage-version-migration"
+eventing_image "mt-broker-controller__mt-broker-controller"  "${eventing}-mtchannel-broker"
+eventing_image "mt-broker-filter__filter"                    "${eventing}-mtbroker-filter"
+eventing_image "mt-broker-ingress__ingress"                  "${eventing}-mtbroker-ingress"
+eventing_image "imc-controller__controller"                  "${eventing}-channel-controller"
+eventing_image "imc-dispatcher__dispatcher"                  "${eventing}-channel-dispatcher"
+
+eventing_image "v0.17.0-pingsource-cleanup__pingsource" "${eventing}-pingsource-cleanup"
+eventing_image "PING_IMAGE"           "${eventing}-ping"
+eventing_image "MT_PING_IMAGE"        "${eventing}-mtping"
+eventing_image "APISERVER_RA_IMAGE"   "${eventing}-apiserver-receive-adapter"
+eventing_image "BROKER_INGRESS_IMAGE" "${eventing}-broker-ingress"
+eventing_image "BROKER_FILTER_IMAGE"  "${eventing}-broker-filter"
+eventing_image "DISPATCHER_IMAGE"     "${eventing}-channel-dispatcher"
+
+kafka_image "kafka-controller-manager__manager"    "${eventing_contrib}-kafka-source-controller"
+kafka_image "KAFKA_RA_IMAGE"                       "${eventing_contrib}-kafka-source-adapter"
+kafka_image "kafka-ch-controller__controller"      "${eventing_contrib}-kafka-channel-controller"
+kafka_image "DISPATCHER_IMAGE"                     "${eventing_contrib}-kafka-channel-dispatcher"
+kafka_image "kafka-ch-dispatcher__dispatcher"      "${eventing_contrib}-kafka-channel-dispatcher"
+kafka_image "kafka-webhook__kafka-webhook"         "${eventing_contrib}-kafka-channel-webhook"
 
 declare -A values
 values[spec.version]="$(metadata.get project.version)"
@@ -71,25 +89,33 @@ values['metadata.annotations[olm.skipRange]']="$(metadata.get olm.skipRange)"
 values[spec.minKubeVersion]="$(metadata.get requirements.kube.minVersion)"
 values[spec.replaces]="$(metadata.get project.name).v$(metadata.get olm.replaces)"
 
-function add_image {
+function add_related_image {
   cat << EOF | yq write --inplace --script - "$1"
 - command: update
   path: spec.relatedImages[+]
   value:
-    name: "IMAGE_${2}"
-    image: "${3}"
+    name: "IMAGE_${2}_${3}"
+    image: "${4}"
 EOF
+}
 
+function add_downstream_operator_deployment_image {
   cat << EOF | yq write --inplace --script - "$1"
-- command: update 
+- command: update
   path: spec.install.spec.deployments(name==knative-openshift).spec.template.spec.containers[0].env[+]
   value:
-    name: "IMAGE_${2}"
-    value: "${3}"
+    name: "IMAGE_${2}_${3}"
+    value: "${4}"
 EOF
+}
 
+# since we also parse the environment variables in the upstream (actually midstream) operator,
+# we don't add scope prefixes to image overrides here. We don't have a clash anyway without any scope prefixes!
+# there was a naming clash between eventing and kafka, but we won't provide the Kafka overrides to the
+# midstream operator.
+function add_upstream_operator_deployment_image {
   cat << EOF | yq write --inplace --script - "$1"
-- command: update 
+- command: update
   path: spec.install.spec.deployments(name==knative-operator).spec.template.spec.containers[0].env[+]
   value:
     name: "IMAGE_${2}"
@@ -100,9 +126,25 @@ EOF
 # Start fresh
 cp "$template" "$target"
 
-for name in "${images[@]}"; do
-  echo "Image: ${name} -> ${images_addresses[$name]}"
-  add_image "$target" "$name" "${images_addresses[$name]}"
+for name in "${serving_images[@]}"; do
+  echo "serving Image: ${name} -> ${serving_images_addresses[$name]}"
+  add_related_image "$target" "SERVING" "$name" "${serving_images_addresses[$name]}"
+  add_downstream_operator_deployment_image "$target" "SERVING" "$name" "${serving_images_addresses[$name]}"
+  add_upstream_operator_deployment_image "$target" "$name" "${serving_images_addresses[$name]}"
+done
+
+for name in "${eventing_images[@]}"; do
+  echo "eventing Image: ${name} -> ${eventing_images_addresses[$name]}"
+  add_related_image "$target" "EVENTING" "$name" "${eventing_images_addresses[$name]}"
+  add_downstream_operator_deployment_image "$target" "EVENTING" "$name" "${eventing_images_addresses[$name]}"
+  add_upstream_operator_deployment_image "$target" "$name" "${eventing_images_addresses[$name]}"
+done
+
+# don't add Kafka image overrides to upstream operator
+for name in "${kafka_images[@]}"; do
+  echo "kafka Image: ${name} -> ${kafka_images_addresses[$name]}"
+  add_related_image "$target" "KAFKA" "$name" "${kafka_images_addresses[$name]}"
+  add_downstream_operator_deployment_image "$target" "KAFKA" "$name" "${kafka_images_addresses[$name]}"
 done
 
 for name in "${!values[@]}"; do
